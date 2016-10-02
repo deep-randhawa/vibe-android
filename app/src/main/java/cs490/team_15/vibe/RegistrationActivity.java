7d51f72a97f0cca247a6cdc329c94c4f76e6a032
deweya <deweya964@gmail.com>
2016-10-02 00:07:33 -0400

    Created new RegistrationActivity

--- app/src/main/java/cs490/team_15/vibe/RegistrationActivity.java
+++ app/src/main/java/cs490/team_15/vibe/RegistrationActivity.java
@@ -3,11 +3,11 @@ package cs490.team_15.vibe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration); // Replace this with the DJ search activity (When implemented)
+        setContentView(R.layout.activity_registration);
     }
 }
